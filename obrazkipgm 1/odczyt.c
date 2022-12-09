ef134d0576355f9a99bd5464550f4b6316a97f38
Wiktor Kwiatkowski <275423@student.pwr.edu.pl>
2022-12-10 00:00:52 +0100

    chybakoniec

--- obrazkipgm 1/odczyt.c
+++ obrazkipgm 1/odczyt.c
@@ -7,13 +7,13 @@
 
 /************************************************************************************
  * Funkcja wczytuje obraz PGM z pliku do tablicy       	       	       	       	    *
- *										    *
- * \param[in] plik_we uchwyt do pliku z obrazem w formacie PGM			    *
- * \param[out] obraz_pgm tablica, do ktorej zostanie zapisany obraz		    *
- * \param[out] wymx szerokosc obrazka						    *
- * \param[out] wymy wysokosc obrazka						    *
- * \param[out] odcienie liczba odcieni odcienie					    *
- * \return liczba wczytanych pikseli						    *
+ *										                                                              *
+ * \param[in] plik_we uchwyt do pliku z obrazem w formacie PGM			                *
+ * \param[out] obraz_pgm tablica, do ktorej zostanie zapisany obraz		              *
+ * \param[out] wymx szerokosc obrazka						                                    *
+ * \param[out] wymy wysokosc obrazka						                                    *
+ * \param[out] odcienie liczba odcieni odcienie					                            *
+ * \return liczba wczytanych pikseli						                                    *
  ************************************************************************************/
 
 int czytaj(FILE *plik_we, int obraz_pgm[][MAX], int *wymx, int *wymy, int *odcienie)
@@ -97,34 +97,34 @@ void negatyw(int obraz[][MAX], int wymx, int wymy, int odcienie)
   {
     for (j = 0; j < wymx; j++)
     {
-      obraz[i][j] = odcienie - obraz[i][j];
+      obraz[i][j] = odcienie - obraz[i][j]; // w pętli zmieniami wartości danych wejściowych na podstawie podanego wzoru
     }
   }
 }
 
-//funkcja zajmująca się progowaniem obrazu
+// funkcja zajmująca się progowaniem obrazu na 50%
 void progowanie(int obraz[][MAX], int wymx, int wymy, int odcienie)
 {
   int i, j;
-  int prog = odcienie / 2;
+  int prog = odcienie / 2; // tworzymy próg na podstawie maksymalnej szarości
 
   for (i = 0; i < wymy; i++)
   {
     for (j = 0; j < wymx; j++)
     {
-      if (obraz[i][j] <= prog)
+      if (obraz[i][j] <= prog) // wartością poniżej 50% szarości przypisiwane jest 0
       {
         obraz[i][j] = 0;
       }
       else
       {
-        obraz[i][j] = odcienie;
+        obraz[i][j] = odcienie; // dla wartości powyżej progi przypisywana jest szarość maksymalna
       }
     }
   }
 }
-
-void progowanie_czerni(int obraz[][MAX], int wymx, int wymy, int odcienie)
+// funkcja zajmująca sie pół progowaniem czerni
+void polprogowanie_czerni(int obraz[][MAX], int wymx, int wymy, int odcienie)
 {
   int i, j;
   int prog = odcienie / 2;
@@ -132,15 +132,13 @@ void progowanie_czerni(int obraz[][MAX], int wymx, int wymy, int odcienie)
   {
     for (j = 0; j < wymx; j++)
     {
-      if (obraz[i][j] <= prog)
+      if (obraz[i][j] <= prog) // wartością poniżej 50% szarości przypisiwane jest 0
         obraz[i][j] = 0;
       else
-        obraz[i][j] = obraz[i][j];
+        obraz[i][j] = obraz[i][j]; // wartości powyżej 50% szarości pozostają bez zmian
     }
   }
 }
-
-
 
 int main()
 {
@@ -154,78 +152,82 @@ int main()
   int opcja;
   int i, j;
 
-  while (opcja != 6) //Proste menu do wyboru przez użytkownija za pomoca switcha
-  {
-    printf("Powiedz mi co chcesz zrobić ze swoim obrazem\n\n");
+  while (opcja != 6) // Proste menu do wyboru przez użytkownika za pomoca switcha, wybranie cyfry 6 kończy działanie programu
+  { printf("\n\n");
+    printf("Powiedz mi co chcesz zrobić ze swoim obrazem, wybierz jedną z opcji poniżej.\n");
+    printf("Pamiętaj o końcówce .pgm !\n\n");
     printf("Proste menu: \n");
-    printf("1-Wyświetl obraz który chcesz zmienić pamiętaj o .pgm!\n");
-    printf("2-Wykonaj progowanien\n");
+    printf("1-Chce podać nazwę obrazu i go wyświetlić\n");
+    printf("2-Wykonaj progowanie\n");
     printf("3-Wykonaj negatyw\n");
     printf("4-Wykonaj półprogowanie czerni\n");
-    printf("5-Zapisz,wyświetl i zakończ\n");
-    printf("6-Nie chce nic robić\n\n");
+    printf("5-Chce zobaczyć mój nowy obraz, zapisać go i wyjść\n");
+    printf("6-Nie chce dzisiaj nic przerabiać\n\n");
     printf("Chce wybrać opcje numer: ");
     scanf("%d", &opcja);
     switch (opcja)
     {
-    case 1:
+    case 1: // opcja nr 1. Wyświetlenie podanego przez użytkownika obrazu
     {
 
-      /*Wczytanie zawartosci wskazanego pliku do pamieci */
+      // Wczytanie zawartosci wskazanego pliku do pamieci
       printf("Podaj nazwe pliku:\n");
       scanf("%s", nazwa);
       plik = fopen(nazwa, "r");
 
       if (plik != NULL)
-      { /* co spowoduje zakomentowanie tego warunku */
+      { // co spowoduje zakomentowanie tego warunku */
         odczytano = czytaj(plik, obraz, &wymx, &wymy, &odcienie);
         fclose(plik);
       }
-      /* Wyswietlenie poprawnie wczytanego obraza zewnetrznym programem */
+      // Wyswietlenie poprawnie wczytanego obraza zewnetrznym programem
       if (odczytano != 0)
       {
         wyswietl(nazwa);
       }
       break;
     }
-    case 2:
+    case 2: // opcja nr 2. progowanie obrazu wczytanego do programu
     {
       progowanie(obraz, wymx, wymy, odcienie);
       printf("Przetwarzam obraz\n\n\n");
       break;
     }
-    case 3:
+    case 3: // opcja nr 3. negatyw obrazu wczytanego do programu
     {
       negatyw(obraz, wymx, wymy, odcienie);
       printf("Przetwarzam obraz\n\n\n");
       break;
     }
-    case 4:
+    case 4: // opcja nr 4. półprogowanie czerni w obrazie wczytanym do programu
     {
-      progowanie_czerni(obraz, wymx, wymy, odcienie);
+      polprogowanie_czerni(obraz, wymx, wymy, odcienie);
       printf("Przetwarzam obraz\n\n\n");
       break;
     }
-    case 5:
+    case 5: // opcja nr 5. zapis pod wskazaną przez użytkownika nazwą i zapis
     {
       printf("Podaj nazwę pod jaką chcesz zapisać swój nowy plik .pgm:\n");
-      scanf("%s", nowanazwa);
-      plik = fopen(nowanazwa, "w");
-      fprintf(plik, "P2\n");
-      fprintf(plik, "%d %d\n%d\n", wymx, wymy, odcienie);
+      scanf("%s", nowanazwa);                             // zapis nazwy podanej przez użytkownika do tablicy
+      plik = fopen(nowanazwa, "w");                       // tworzenie pliku o podanej nazwie
+      fprintf(plik, "P2\n");                              // wpisanie do nowo utworzonego pliku, magicznego P2,
+      fprintf(plik, "%d %d\n%d\n", wymx, wymy, odcienie); // rozmiarów i szarości
       for (i = 0; i < wymy; i++)
       {
         for (j = 0; j < wymx; j++)
         {
-          fprintf(plik, " %d ", obraz[i][j]);
-        }
+          fprintf(plik, " %d ", obraz[i][j]);             // podmienie orginalnych wartości przez funkcje która była
+        }                                                 // użyta w menu wyboru
         fprintf(plik, "\n");
       }
       wyswietl(nowanazwa);
       return odczytano;
       break;
+      return odczytano;
+      break;
     }
     case 6:
+    printf("Mam nadzieje, że jeszcze kiedyś się zobaczymy :)\n\n");
     }
   }
 }
